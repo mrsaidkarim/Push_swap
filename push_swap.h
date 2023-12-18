574451bf814746b14e2304c8a8414627b2281cd6
SaidKarim-web <sirsaidkarim@gmail.com>
2023-12-18 21:07:44 +0100

    Add main files

---
+++ push_swap.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 18:35:27 by skarim            #+#    #+#             */
+/*   Updated: 2023/12/18 15:37:59 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <stddef.h>
+# include <limits.h>
+# include <stdlib.h>
+
+// stack
+typedef struct s_stack_node
+{
+	int					value;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*prev;
+	struct s_stack_node *next;
+} t_stack_node;
+
+// functions
+char	**ft_split(char *s, char c);
+#endif
---
+++ split.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 19:23:00 by skarim            #+#    #+#             */
+/*   Updated: 2023/12/18 16:28:27 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	ft_count(char *s, char c)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+			count++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (count);
+}
+
+static char	*ft_word(char *str, char c)
+{
+	int		end;
+	int		i;
+	char	*s;
+
+	end = 0;
+	while (str[end] && str[end] != c)
+		end++;
+	s = (char *)malloc(sizeof(char) * (end + 1));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (i < end)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[end] = '\0';
+	return (s);
+}
+
+static char	**ft_free(char **s, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);
+	return (NULL);
+}
+
+char	**ft_split(char *s, char c)
+{
+	char	**res;
+	int		i;
+	int		count;
+
+	count = ft_count(s, c);
+	res = (char **)malloc(sizeof(char *) * (count + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	res[count] = NULL;
+	while (i < count)
+	{
+		while (*s && *s == c)
+			s++;
+		res[i] = ft_word(s, c);
+		if (res[i] == NULL)
+			return (ft_free(res, i));
+		while (*s && *s != c)
+			s++;
+		i++;
+	}
+	return (res);
+}
+
+// #include <stdio.h>
+// int main(int argc, char **argv)
+// {
+// 	char **res;
+// 	res = ft_split(argv[1], ' ');
+// 	while (*res)
+// 	{
+// 		printf("|%s|", *res);
+// 		res++;
+// 	}
+// 	return (0);
+// }
