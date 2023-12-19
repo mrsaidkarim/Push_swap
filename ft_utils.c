1e6dc9d62085c157a101264dc29adc1d775eff2f
SaidKarim-web <sirsaidkarim@gmail.com>
2023-12-19 21:59:42 +0100

    Add ft_utils

---
+++ ft_utils.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 17:02:03 by skarim            #+#    #+#             */
+/*   Updated: 2023/12/19 17:47:59 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+long long	ft_atoi(char *s)
+{
+	int			i;
+	long long	n;
+	int			sign;
+
+	i = 0;
+	sign = 1;
+	n = 0;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (s[i])
+	{
+		n = n * 10 + (s[i] - '0');
+		i++;
+	}
+	return (n * sign);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
