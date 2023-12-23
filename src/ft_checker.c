4a6f1ca3b665e3c500cb9cdcacf16fbd9435fa7b
SaidKarim-web <sirsaidkarim@gmail.com>
2023-12-23 16:36:35 +0100

    Add checker function

--- src/ft_checker.c
+++ src/ft_checker.c
@@ -6,12 +6,11 @@
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 17:46:55 by skarim            #+#    #+#             */
-/*   Updated: 2023/12/19 18:32:54 by skarim           ###   ########.fr       */
+/*   Updated: 2023/12/20 19:15:04 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
 
 int	ft_arg_checker(char *s)
 {
@@ -32,5 +31,37 @@ int	ft_arg_checker(char *s)
 	}
 	if (checked == 0 || ft_atoi(s) > INT_MAX || ft_atoi(s) < INT_MIN)
 		return (0);
+	return (1);
+}
+
+int	ft_dup(t_stack_node *a)
+{
+	t_stack_node	*tmp;
+
+	if (a)
+	{
+		while (a)
+		{
+			tmp = a->next;
+			while (tmp)
+			{
+				if (tmp->value == a->value)
+					return (1);
+				tmp = tmp->next;
+			}
+			a = a->next;
+		}
+	}
+	return (0);
+}
+
+int	ft_sorted(t_stack_node *a)
+{
+	while (a && a->next)
+	{
+		if (a->value > a->next->value)
+			return (0);
+		a = a->next;
+	}
 	return (1);
 }
